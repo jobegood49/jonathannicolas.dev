b6b71de9cc83f181ffe7cd7e98fe26e0a5933ee9
jobegood49 <jonathan.s.nicolas@gmail.com>
2020-03-04 20:40:42 -0800

    Add style to Hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,19 +1,64 @@
 import React from "react";
 import styled from "styled-components";
 
+const HeroContainer = styled.div`
+  min-height: 755px;
+  background-color: #6b95ff;
+  display: flex;
+  align-items: center;
+`;
+
+const HeroSubContainer = styled.div`
+  display: flex;
+  margin: 0 auto;
+  width: 1400px;
+  justify-content: space-around;
+`;
+
+const HeroIntro = styled.div`
+  min-width: 400px;
+  margin: 0px 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  & img {
+    border-radius: 50%;
+  }
+
+  & h1 {
+    width: 350px;
+    text-align: center;
+  }
+  /* buttons group */
+  & div {
+    display: flex;
+  }
+  & button {
+    font-size: 1.3rem;
+    background: #56d0d8;
+    border-radius: 10px;
+    margin: 30px 10px;
+    padding: 10px 20px;
+    border: none;
+  }
+`;
+
 const Hero = () => {
   return (
-    <div>
-      <div>
-        <img src="/images/avatar.jpg" alt="Avatar of Jonathan Nicolas" />
-        <h1>Hello, I'm Jonathan Nicolas, a web developer</h1>
-        <div>
-          <button>Contact Me</button>
-          <button>Check My Projects</button>
-        </div>
+    <HeroContainer>
+      <HeroSubContainer>
+        <HeroIntro>
+          <img src="/images/avatar.jpg" alt="Avatar of Jonathan Nicolas" />
+          <h1>Hello, I'm Jonathan Nicolas, a web developer</h1>
+          <div>
+            <button>Contact Me</button>
+            <button>Check My Projects</button>
+          </div>
+        </HeroIntro>
         <img src="/images/hero-image.svg" alt="Hero Image" />
-      </div>
-    </div>
+      </HeroSubContainer>
+    </HeroContainer>
   );
 };
 
