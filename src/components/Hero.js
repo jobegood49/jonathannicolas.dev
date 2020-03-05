fcf6040bbfb375c4d22c04a090080a94b3dd4603
jobegood49 <jonathan.s.nicolas@gmail.com>
2020-03-04 19:58:39 -0800

    Add content of Hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,7 +2,19 @@ import React from "react";
 import styled from "styled-components";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div>
+      <div>
+        <img src="/images/avatar.jpg" alt="Avatar of Jonathan Nicolas" />
+        <h1>Hello, I'm Jonathan Nicolas, a web developer</h1>
+        <div>
+          <button>Contact Me</button>
+          <button>Check My Projects</button>
+        </div>
+        <img src="/images/hero-image.svg" alt="Hero Image" />
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
